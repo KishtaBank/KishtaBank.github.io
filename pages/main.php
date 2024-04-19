d44c1fbc8b55b21077660430bc52880140743608
1000ShakeDev <136198633+1000ShakeDev@users.noreply.github.com>
2024-04-19 17:14:59 +0200

    Update
    + page de redirection
    + rangement dans les fichiers

---
+++ pages/main.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Kishta Bank</title>
+
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+
+    <!-- En-tête -->
+    <header>
+
+    </header>
+
+    <!-- Partie principale -->
+    <main>
+        
+    </main>
+
+    <!-- Pied de page -->
+    <footer>
+        <p>&copy; 2024 KishtaBank Banque centrale. Tous droits réservés.</p>
+    </footer>
+</body>
+</html>
